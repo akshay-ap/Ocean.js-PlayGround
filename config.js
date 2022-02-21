c84815acf646c8970d526bd9b52b087f8ad882ee
Akshay <akshay.ap95@gmail.com>
2022-02-21 16:31:47 +0100

    Create V4 example

--- config.js
+++ config.js
@@ -1,30 +1,20 @@
-const { ConfigHelper } = require("@oceanprotocol/lib");
-const Web3 = require("web3");
-const defaultConfig = new ConfigHelper().getConfig("development");
+require('dotenv').config()
+const networkUrl = process.env.networkUrl
+const aquarius = process.env.aquarius
+const providerUri = process.env.providerUri
 
 const urls = {
-  networkUrl: "http://localhost:8545",
-  aquarius: "http://localhost:5000",
-  providerUri: "http://localhost:8030",
+  networkUrl: networkUrl,
+  aquarius: aquarius,
+  providerUri: providerUri,
 };
 
-const contracts = {
-  DTFactory: "0x1e6d9207241DbDca82B0D9546490c97B24B1a9f6",
-  BFactory: "0x98b6901cE7C9fc65dBeeC98598136593EB7b4c6C",
-  FixedRateExchange: "0x91EB42b164664cB28a09B0cF9651b404Ee105afA",
-  Metadata: "0x9C2a015129969c98E9A5BcBEb61A5F907FF5b629",
-  Ocean: "0x2fC1fd21cb222Dc180Ef817dE4c426fd9230b5A5",
-};
-
-const config = {
-  ...defaultConfig,
-  metadataCacheUri: urls.aquarius,
-  providerUri: urls.providerUri,
-  web3Provider: new Web3(urls.networkUrl),
+const walletConfig = {
+  mnemonic: process.env.MNEMONIC,
+  rpc: networkUrl,
 };
 
 module.exports = {
-  config,
-  contracts,
   urls,
+  walletConfig
 };
--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const dummyAsset = {
+const dummyDataAsset = {
   main: {
     type: "dataset",
     name: "test-dataset",
@@ -7,12 +7,79 @@ const dummyAsset = {
     license: "MIT",
     files: [
       {
-        url:
-          "https://file-examples-com.github.io/uploads/2017/02/file_example_XLS_10.xls",
-        contentType: "xlsx",
-      },
+        url: "https://raw.githubusercontent.com/trentmc/branin/main/branin.arff",
+        index: 0,
+        contentType: "text/text"
+      }
     ],
   },
 };
 
-module.exports = { dummyAsset };
+const dummyAlgoAsset = {
+  main: {
+    type: 'algorithm',
+    name: 'Test Algo with Compute',
+    dateCreated: new Date(Date.now()).toISOString().split(".")[0] + "Z",
+    datePublished: new Date(Date.now()).toISOString().split(".")[0] + "Z",
+    author: 'DevOps',
+    license: 'CC-BY',
+    files: [
+      {
+        url: 'https://raw.githubusercontent.com/oceanprotocol/test-algorithm/master/javascript/algo.js',
+        contentType: 'text/js',
+        encoding: 'UTF-8'
+      }
+    ],
+    algorithm: {
+      language: 'js',
+      format: 'docker-image',
+      version: '0.1',
+      container: {
+        entrypoint: 'node $ALGO',
+        image: 'node',
+        tag: '10'
+      }
+    }
+  }
+};
+
+const genericAsset = {
+  '@context': ['https://w3id.org/did/v1'],
+  id: 'testFakeDid',
+  version: '4.0.0',
+  chainId: 4,
+  nftAddress: '0x0',
+  metadata: {
+    created: '2021-12-20T14:35:20Z',
+    updated: '2021-12-20T14:35:20Z',
+    name: 'dataset-name',
+    type: 'dataset',
+    description: 'Ocean protocol test dataset description',
+    author: 'oceanprotocol-team',
+    license: 'MIT',
+    tags: ['white-papers'],
+    additionalInformation: { 'test-key': 'test-value' },
+    links: ['http://data.ceda.ac.uk/badc/ukcp09/']
+  },
+  services: [
+    {
+      id: 'testFakeId',
+      type: 'access',
+      description: 'Download service',
+      files: '',
+      datatokenAddress: '0xa15024b732A8f2146423D14209eFd074e61964F3',
+      serviceEndpoint: 'https://providerv4.rinkeby.oceanprotocol.com',
+      timeout: 0
+    }
+  ]
+}
+
+const files = [
+  {
+    type: 'url',
+    url: 'https://raw.githubusercontent.com/oceanprotocol/testdatasets/main/shs_dataset_test.txt',
+    method: 'GET'
+  }
+]
+
+module.exports = { dummyAsset: dummyDataAsset, dummyAlgoAsset, genericAsset, files };
