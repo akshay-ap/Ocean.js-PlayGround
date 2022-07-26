ff2f03ab01e0c4187dde1a634847ff920fedafae
Akshay <akshay.ap95@gmail.com>
2022-07-26 12:04:00 +0200

    Update example

--- 2.2.updateMetadata.js
+++ 2.2.updateMetadata.js
@@ -7,40 +7,31 @@ const {
 const { SHA256 } = require('crypto-js');
 const Web3 = require('web3');
 const { web3Provider, oceanConfig } = require('./config');
-const { ddo, files } = require('./data');
 
 const web3 = new Web3(web3Provider);
 const aquarius = new Aquarius(oceanConfig.metadataCacheUri);
 const nft = new Nft(web3);
 const providerUrl = oceanConfig.providerUri;
 
-const dataNFTAddress = "0xF2231f1978363e958750497e75a3449219b03851";
-const datatokenAddress = "0xA07C22622772E72E45370Cc265C547b6af4AFD3A";
+const did = "did:op:a419f07306d71f3357f8df74807d5d12bddd6bcd738eb0b461470c64859d6f0f";
 
-console.log(`DataNft address ${dataNFTAddress}`);
-console.log(`Datatoken address ${datatokenAddress}`);
-
-const setMetadata = async (erc721Address, datatokenAddress) => {
+const setMetadata = async (did) => {
   const accounts = await web3.eth.getAccounts();
   const publisherAccount = accounts[0];
 
-  // create the files encrypted string
-  let providerResponse = await ProviderInstance.encrypt(files, providerUrl);
-  ddo.services[0].files = providerResponse;
-  ddo.services[0].datatokenAddress = datatokenAddress;
-  // update ddo and set the right did
-  ddo.nftAddress = erc721Address;
-  const chain = await web3.eth.getChainId();
-  ddo.id = `did:op:${SHA256(
-    web3.utils.toChecksumAddress(erc721Address) + chain.toString(10)
-  )}`;
+  const ddo = await aquarius.resolve(did);
+
+  // update the ddo here
+  ddo.metadata.name = "Sample dataset v2";
+  ddo.metadata.description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam";
+  ddo.metadata.tags = ["new tag1", "new tag2"];
 
   providerResponse = await ProviderInstance.encrypt(ddo, providerUrl);
   const encryptedResponse = await providerResponse;
   const metadataHash = getHash(JSON.stringify(ddo));
 
   await nft.setMetadata(
-    erc721Address,
+    ddo.nftAddress,
     publisherAccount,
     0,
     providerUrl,
@@ -53,15 +44,16 @@ const setMetadata = async (erc721Address, datatokenAddress) => {
   await aquarius.waitForAqua(ddo.id);
 
   console.log(`Resolved asset did [${ddo.id}]from aquarius.`);
+  console.log(`Updated name: [${ddo.metadata.name}].`);
+  console.log(`Updated description: [${ddo.metadata.description}].`);
+  console.log(`Updated tags: [${ddo.metadata.tags}].`);
+
 };
 
 
-setMetadata(dataNFTAddress, datatokenAddress).then(() => {
-  console.log('Metadata set.');
+setMetadata(did).then(() => {
   process.exit();
 }).catch((err) => {
   console.error(err);
   process.exit(1);
-});;
-
-
+});
